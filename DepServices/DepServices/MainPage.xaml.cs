1c596617a8e294340d1aee8b549aa0c360add05b
jopatino <36779409+jopatino@users.noreply.github.com>
2018-06-05 20:27:54 -0500

    FinalFinal

--- DepServices/DepServices/MainPage.xaml.cs
+++ DepServices/DepServices/MainPage.xaml.cs
@@ -15,7 +15,19 @@ namespace DepServices
 			InitializeComponent();
             btnMessage.Clicked += BtnMessage_Clicked;
             btnTextoVoz.Clicked += BtnTextoVoz_Clicked;
+            btnPlataforma.Clicked += BtnPlataforma_Clicked;
 		}
+
+        private async void BtnPlataforma_Clicked(object sender, EventArgs e)
+        {
+            await Task.Delay(5000).ConfigureAwait(false);
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                this.txtAviso.Text = DateTime.Now.ToString());
+                return true;
+            });
+        }
 
         private void BtnTextoVoz_Clicked(object sender, EventArgs e)
         {
